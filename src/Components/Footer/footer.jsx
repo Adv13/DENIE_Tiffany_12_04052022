0d48f1851a5303c87d294c3ea0fb1d0168f6bf0d
Adv <tiffanydpro923@gmail.com>
2022-05-13 19:50:24 +0200

    maj files

--- src/Components/Footer/footer.jsx
+++ src/Components/Footer/footer.jsx
@@ -7,17 +7,17 @@ import Copyright from '../../assets/Copyright2020.png'
 function Footer(){
     return(
     <footer>
-        <form class="form-inline d-flex flex-column">
-            <button class="btn btn-sm btn-outline-secondary bg-light" type="button">
+        <form className="form-inline d-flex flex-column">
+            <button className="btn btn-sm btn-outline-secondary bg-light" type="button">
                 <img src={YogaIcon} alt="Yoga Icon"></img>
             </button>
-            <button class="btn btn-sm btn-outline-secondary bg-light" type="button">
+            <button className="btn btn-sm btn-outline-secondary bg-light" type="button">
                 <img src={SwimmingIcon} alt="Swimming Icon"></img>
             </button>
-            <button class="btn btn-sm btn-outline-secondary bg-light" type="button">
+            <button className="btn btn-sm btn-outline-secondary bg-light" type="button">
                 <img src={BikeIcon} alt="Bike Icon"></img>
             </button>
-            <button class="btn btn-sm btn-outline-secondary bg-light" type="button">
+            <button className="btn btn-sm btn-outline-secondary bg-light" type="button">
                 <img src={TrainingIcon} alt="Training Icon"></img>
             </button>
         </form>
