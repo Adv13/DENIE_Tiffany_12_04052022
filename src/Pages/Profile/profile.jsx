c02f478ba3b9ddc6b9d273b86b290139de0a5e13
Tanarys <tiffanydpro923@gmail.com>
2022-05-16 15:57:39 +0200

    first use of recharts

--- src/Pages/Profile/profile.jsx
+++ src/Pages/Profile/profile.jsx
@@ -1,11 +1,70 @@
+import {
+    LineChart,
+    ResponsiveContainer,
+    Legend, Tooltip,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid
+} from 'recharts';
 
-
-
+// Sample chart data
+const pdata = [
+    {
+        name: 'MongoDb',
+        student: 11,
+        fees: 120
+    },
+    {
+        name: 'Javascript',
+        student: 15,
+        fees: 12
+    },
+    {
+        name: 'PHP',
+        student: 5,
+        fees: 10
+    },
+    {
+        name: 'Java',
+        student: 10,
+        fees: 5
+    },
+    {
+        name: 'C#',
+        student: 9,
+        fees: 4
+    },
+    {
+        name: 'C++',
+        student: 10,
+        fees: 8
+    },
+];
 
 function Profile(){
     return(<section>
         <h1>Bonjour <strong>Thomas</strong></h1>
         <h2>Félicitation ! Vous avez explosé vos objectifs hier 👏</h2>
+        <>
+            <h1 className="text-heading">
+                Line Chart Using Rechart
+            </h1>
+            <ResponsiveContainer width="100%" aspect={3}>
+                <LineChart data={pdata} margin={{ right: 300 }}>
+                    <CartesianGrid />
+                    <XAxis dataKey="name" 
+                        interval={'preserveStartEnd'} />
+                    <YAxis></YAxis>
+                    <Legend />
+                    <Tooltip />
+                    <Line dataKey="student"
+                        stroke="black" activeDot={{ r: 8 }} />
+                    <Line dataKey="fees"
+                        stroke="red" activeDot={{ r: 8 }} />
+                </LineChart>
+            </ResponsiveContainer>
+        </>
         </section>)
 }
 
