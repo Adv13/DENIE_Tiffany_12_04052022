a5bdec503154581e35a46c34c7b22fe8c50e9cbf
Tanarys <tiffanydpro923@gmail.com>
2022-05-24 13:09:48 +0200

    jest test fixed problem on laptop and first test done

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,43 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+/**
+ * @jest-environment jsdom
+ */
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { fireEvent, render } from '@testing-library/react';
+import React from 'react';
+import { Router } from 'react-router-dom';
+import ButtonLogin from './ButtonLogin';
+import { createMemoryHistory } from 'history';
+
+const { render } = require("@testing-library/react");
+const { createMemoryHistory } = require("history");
+const React = require("react");
+const { Router } = require("react-router-dom");
+
+test('use jsdom in this test file', () => {
+  const element = document.createElement('div');
+  expect(element).not.toBeNull();
+});
+
+it("works", () => {
+  const history = createMemoryHistory();
+  render(
+    <Router location={history.location} navigator={history}></Router>
+  );
+});
+
+
+
+
+describe('ButtonLogin', () => {
+  test('should pass', () => {
+    const history = createMemoryHistory({ initialEntries: ['/home'] });
+    const { getByText } = render(
+      <Router history={history}>
+        <ButtonLogin />
+      </Router>
+    );
+    expect(history.location.pathname).toBe('/home');
+    fireEvent.click(getByText('Iniciar sesión'));
+    expect(history.location.pathname).toBe('/login');
+  });
 });
