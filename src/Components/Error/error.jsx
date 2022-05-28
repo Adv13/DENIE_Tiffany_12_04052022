a967379fd08a6e2cea233c7a484dfd783db4797a
Tanarys <tiffanydpro923@gmail.com>
2022-05-28 17:20:51 +0200

    error component when no data done

--- src/Components/Error/error.jsx
+++ src/Components/Error/error.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 function Error(){
     return(
-    <div className="wrapper">
+    <div className="wrapper d-flex flex-column align-items-center">
     <h1>404</h1>
-    <h2>Oups! La page que vous demandez n'existe pas</h2>
+    <h2>Oups! La page que vous demandez n'existe pas.</h2>
     <div className="err-back">
-      <Link className="backlink" to="/">
+      <Link className="backlink text-danger" to="/">
         Retourner sur la page d'accueil
       </Link>
     </div>
--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('First render test', () => {
   render(<App />);
   expect(true).toStrictEqual(true);
-
-
 });
+
